7681b3863292247d07afa6da4c16359c0d9730c4
Pramod <cpramod2012@gmail.com>
2023-12-06 23:03:11 +0545

    update php server for development

--- server.php
+++ server.php
@@ -1,12 +1,17 @@
+#!/usr/bin/php
 <?php
-#!/bin/php
 class Server
 {
     public $os, $config, $web_root_path;
     public $port = "8080";
-    public $php = "7.4";
-    public $daemon = false;
-    public $live_site = "https://levhatorah.org"; // live site link, used load live site images to local
+    public $php = "8.1";
+    
+    public $worker = 20;
+	public $routes = "";
+
+    public $phpini = true; // false if to use default php.ini or path to php.ini
+    public $live_site = "https://google.com"; // live site link, used load live site images to local
+    public $configPath = "";
 
     function __construct()
     {
@@ -14,52 +19,21 @@ class Server
         // get project root directory path
         $this->web_root_path = dirname(__FILE__);
         $this->getOs();
-        $this->configJson();
-
-    }
-
-    /**
-     * install php unit for selected version
-     */
-    public function installPhpUnit(){
-        //Latest version of Unit as of this release
-        $ver="1.31.1";
-        exec("wget https://unit.nginx.org/download/unit-{$ver}.tar.gz");
-        exec("tar xzf unit-$".$ver.".tar.gz");
-        exec("cd unit-".$ver);
-        exec("apt-get -y install libssl-dev");
-
-        exec("./configure --prefix=/usr --state=/var/lib/unit --control=unix:/var/run/control.unit.sock \
-    --pid=/var/run/unit.pid --log=/var/log/unit.log --tmp=/var/tmp --user=unit --group=unit \
-    --tests --openssl --modules=/usr/lib/unit/modules --libdir=/usr/lib/x86_64-linux-gnu");
-
-        exec("./configure php --module=php".$this->php." --config=php-config --lib-path=/usr/lib");
-        exec("make php".$this->php);
-        exec("make install");
+        $this->getRoutes();
+        $this->server();
     }
 
     public function initalSetup(){
-        exec('sudo pkill unitd');
-        $php = exec("which php".$this->php);
-        if(!$php){
-            if($this->os == 'linux'){
-                // install php version
-                echo exec("sudo apt-get update && apt -y install software-properties-common && add-apt-repository ppa:ondrej/php && apt-get update && apt -y install php".$this->php);
-                // install php dev and embed
-                echo exec("sudo apt install php".$this->php."-dev libphp".$this->php."-embed");
-
-                #install php-unit
-                $this->installPhpUnit();
+        $this->configPath = dirname(__FILE__).'/.server';
+        if(!is_dir($this->configPath)){
+            mkdir($this->configPath,0755, true);
+        }
+        if($this->phpini != false){
+            $this->phpini = $this->configPath.'/php.ini';
+            if(!file_exists($this->phpini)){
+                file_put_contents($this->phpini,'');
             }
         }
-
-        $php_ver = exec($php .' -v');
-        if(strpos($this->php,$php_ver) > 0){
-
-        }else{
-            //exec("sudo update-alternatives --set php /usr/bin/php".$this->php);
-        }
-
     }
 
     public function getOs(){
@@ -72,87 +46,26 @@ class Server
         endif;
     }
 
-    public function configJson(){
-        $php = $this->php ? 'php'.$this->php :'';
-        $this->config = '
-        {
-            "listeners": {
-                "*:'.$this->port.'": {
-                    "pass": "routes"
-                }
-            },
+    public function getRoutes(){
+        $route_content = '<?php
 
-            "routes": [
-            {
-                "match": {
-                    "uri": [
-                        "*.php",
-                        "*.php/*",
-                        "/wp-admin/",
-                        "!/wp-content/uploads/"
-                    ]
-                },
-
-                "action": {
-                    "pass": "applications/wordpress/direct"
-                }
-            },
-            {
-                "match":{
-                    "uri":[
-                        "/wp-content/uploads/*"
-                    ]
-                },
-                "action":{
-                    "share": "siteurl$uri",
-                    "fallback":{
-                        "return":301,
-                        "location":"website$uri"
-                    }
-                }
-            },
-            {
-                "action": {
-                    "share": "siteurl$uri",
-                    "fallback": {
-                        "pass": "applications/wordpress/index"
-                    }
-                }
-            }],
-
-            "applications": {
-                "wordpress": {
-                    "type": "'.$php.'",
-                    "processes": {
-                        "max": 15,
-                        "spare": 5,
-                        "idle_timeout": 180
-                    },
-                "targets": {
-                    "direct": {
-                        "root": "siteurl"
-                    },
-                "index": {
-                    "root": "siteurl",
-                    "script": "index.php"
-                }
-            },
-            "options": {
-                "admin": {
-                    "file":"siteurl/server/php.ini",
-                    "memory_limit": "512M",
-                    "max_execution_time":"0"
-                },
-
-                "user": {
-                    "display_errors": "1"
-                }
+            if (php_sapi_name() !== "cli-server") {
+                die("this is only for the php development server");
             }
+            elseif (preg_match("/\.(?:png|jpg|jpeg|gif)$/", $_SERVER["REQUEST_URI"])) {';
+        if($this->live_site != ''){
+            $route_content.='header("Location: '.$this->live_site.'".$_SERVER["REQUEST_URI"], true, 301);
+            return false;';
         }
-    }}';
-        $this->config = str_replace('siteurl',$this->web_root_path,$this->config);
-        $this->config = str_replace('website',$this->live_site,$this->config);
+        $route_content.='
+            }
+            // if needed, fix also "PATH_INFO" and "PHP_SELF" variables here...
 
+            // require the entry point
+            require "index.php";
+        ?>';
+        $this->routes = $this->configPath.'/routes.php';
+        file_put_contents($this->routes,$route_content);
     }
 
     /***
@@ -160,79 +73,28 @@ class Server
      * config file for running in linux
      *
      */
-    public function runInLinux(){
-        $socket = "/var/run/control.unit.sock";
-        exec("sudo systemctl stop unit");
-        if(exec('pidof unitd')){
-            echo $flag = exec("sudo pkill unitd");
+    public function runServer(){
+	    $command = "PHP_CLI_SERVER_WORKERS=".$this->worker;
+        $command.=" ENV=local";
+        $php = exec('which php'.$this->php);
+        $command = $command ." ".$php." -S localhost:".$this->port;
+        if($this->phpini != false){
+            $command = $command." -c ".$this->phpini;
         }
 
-        $user = "root";
-        $group = "root";
-        if($this->daemon == false){
-            $daemon_txt = "";
-        }else{
-            $daemon_txt = " --no-daemon ";
-        }
-        echo exec("sudo unitd ".$daemon_txt." --user " . $user . " --group " . $group . " --log /dev/stderr");
-        sleep(2);
-        echo exec("sudo curl -X PUT --data-binary '".$this->config."' --unix-socket " . $socket . " http://localhost/config/");
-    }
-
-    public function runInMac(){
-        $os_type = php_uname();
-        if (strpos('x86_64', $os_type) != -1) {
-            $socket = "/usr/local/var/run/unit/control.sock";
-        } else {
-            $socket = "/opt/homebrew/var/run/unit/control.sock";
-        }
-
-        exec("brew services stop unit");
-        exec("sudo pkill unitd");
-        $user = exec("whoami");
-        $group = "staff";
-        exec("brew services stop unit");
-        exec("sudo pkill unitd");
-        echo exec("unitd --no-daemon --user " . $user . " --group " . $group . " --log /dev/stderr & sleep 2 && curl -X PUT --data-binary @" . $this->web_root_path.'/server.json' . " --unix-socket " . $socket . " http://localhost/config/");
-    }
-
-    private function installInMac(){
-        echo exec("brew update");
-        echo exec('brew install unit');
-        echo exec('brew install unit-php');
-    }
-
-    private function installInLinux(){
-
+        exec($command.' '.$this->routes);
     }
 
     public function server(){
-        // check if nginx unit is installed or not
-        $output = exec("which unitd");
+        // check if apachectl is installed or not
+        $output = exec("which php".$this->php);
         if($output){ //installed
-            file_put_contents($this->web_root_path.'/server.json', $this->config);
-            exec("sudo chmod g+x " . $this->web_root_path); //enable execute permission to project directory
-            if($this->os == 'linux'){
-                $this->runInLinux();
-            }elseif($this->os == 'mac'){
-                $this->runInMac();
-            }
+	        $this->runServer();
         }else{ // not installed
-            if($this->os == 'linux'){
-
-                // install script of unit and unit-php in linux
-                $this->installInLinux();
-            }elseif($this->os == 'mac'){
-
-                // install script of unit and unit-php in mac
-                $this->installInMac();
-            }elseif ($this->os == 'win'){
-                // TODO: install script on windows
-            }
+            echo "Install php version ".$this->php."\n";
         }
         exit;
     }
 }
 
 $server = new Server();
-$server->server();
